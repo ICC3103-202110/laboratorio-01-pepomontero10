2678bc57c99f73bca03e2f2ab3aa6c1f89e67f14
pepomontero10 <jmontero3@miuandes.cl>
2021-03-22 18:11:52 -0300

    Commit 2

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,26 @@
-hola
+import random
+
+print("Cuantas cartas quieres, jugador 1?")
+cartas_1 = int(input())
+
+a = int(cartas_1)
+
+jugador_1 = []
+
+while a != 0:
+    x = random.randint(1,int(cartas_1))
+    if x in jugador_1:
+        jugador_1.remove(x)
+        a += 1
+    jugador_1.append(x)
+    a -= 1
+maso = []
+for i in jugador_1:
+    maso.append(i)
+    maso.append(i)
+print(jugador_1)
+print(maso)
+
+
+
+
