223851641097e04b0e68a7e8f78fbb2c5dedfffe
pepomontero10 <jmontero3@miuandes.cl>
2021-03-23 14:43:38 -0300

    matriz 2

--- Lab1.py
+++ Lab1.py
@@ -1,44 +1,49 @@
 import random
-
-print("Cuantas cartas quieres?")
-cartas_1 = int(input())
-
-a = int(cartas_1)
-
-jugador_1 = []
+print("How many card do you want?")
+cards_1 = int(input())
+a = int(cards_1)
+player_1 = []
 
 while a != 0:
-    x = random.randint(1,int(cartas_1))
-    if x in jugador_1:
-        jugador_1.remove(x)
+    x = random.randint(1,int(cards_1))
+    if x in player_1:
+        player_1.remove(x)
         a += 1
-    jugador_1.append(x)
+    player_1.append(x)
     a -= 1
-maso = []
-for i in jugador_1:
-    maso.append(i)
-    maso.append(i)
+deck = []
+for i in player_1:
+    deck.append(i)
+    deck.append(i)
 
-random.shuffle(maso)
+random.shuffle(deck)
 
-real_maso = []
-b = int((2 * int(cartas_1))**(0.5))
-c = int(b) +1
+real_deck = []
+b = int((2 * int(cards_1))**(0.5))
+c = int(b)+1
 n = 0
-print(len(maso))
 while c != 0:
     row = []
     for i in range(b):
-        row.append(maso[n])
+        if n >= (int(cards_1)*2):
+            break
+        row.append(deck[n])
         n += 1
-        if n == len(maso)-1 :
-            c -= 1
-    real_maso.append(row)
+    real_deck.append(row)
     c -= 1
 
-for i in real_maso:
+if len(row) == 0:
+    real_deck.pop()
+
+game_deck = real_deck[:]
+
+for i in real_deck:
     print(i)
 
+for i in game_deck:
+    for b in i:
+        game_deck[i][b] = "*"
 
-
-
+print ("")
+for i in game_deck:
+    print(i)
